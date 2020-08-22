bccd4b09163e178e50e3439520dfe925b72b76eb
Jason Burbage <kryptx@gmail.com>
2020-08-22 07:07:03 +0000

    Use beforeEach pattern in test

--- writers/mongo_test.go
+++ writers/mongo_test.go
@@ -18,28 +18,36 @@ type MockCollection struct {
 	mock.Mock
 }
 
-func (collection *MockCollection) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+func (collection *MockCollection) InsertOne(
+	ctx context.Context,
+	document interface{},
+	opts ...*options.InsertOneOptions,
+) (
+	*mongo.InsertOneResult,
+	error,
+) {
 	args := collection.Called(ctx, document, opts)
 	return args.Get(0).(*mongo.InsertOneResult), args.Error(1)
 }
 
-func TestMongoWriter_Success_ReturnsNilError(t *testing.T) {
+func setupMocks(result *mongo.InsertOneResult, err error) (writers.MongoTweetWriter, *twitter.Tweet) {
 	collection := new(MockCollection)
 	collection.On("InsertOne", mock.Anything, mock.Anything, mock.Anything).
-		Return(&mongo.InsertOneResult{InsertedID: primitive.ObjectID{1}}, nil)
+		Return(result, err)
 	writer := writers.MongoTweetWriter{Collection: collection}
-	tweet := twitter.Tweet{Text: "Foo"}
-	err := writer.Write(&tweet)
+	tweet := &twitter.Tweet{Text: "Foo"}
+	return writer, tweet
+}
+
+func TestMongoWriter_Success_ReturnsNilError(t *testing.T) {
+	writer, tweet := setupMocks(&mongo.InsertOneResult{InsertedID: primitive.ObjectID{1}}, nil)
+	err := writer.Write(tweet)
 	assert.Nil(t, err)
 }
 
 func TestMongoWriter_Error_ReturnsError(t *testing.T) {
 	errorMessage := "Mock error"
-	collection := new(MockCollection)
-	collection.On("InsertOne", mock.Anything, mock.Anything, mock.Anything).
-		Return(&mongo.InsertOneResult{InsertedID: nil}, errors.New(errorMessage))
-	writer := writers.MongoTweetWriter{Collection: collection}
-	tweet := twitter.Tweet{Text: "Foo"}
-	err := writer.Write(&tweet)
+	writer, tweet := setupMocks(&mongo.InsertOneResult{InsertedID: nil}, errors.New(errorMessage))
+	err := writer.Write(tweet)
 	assert.Equal(t, err.Error(), errorMessage)
 }
